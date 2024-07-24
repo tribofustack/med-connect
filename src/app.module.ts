924754846332ad1485b31839c9e8d119439d0e03
arthursvpb <arthurandroid15@gmail.com>
2024-07-24 20:54:39 -0300

    Add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { AppointmentModule } from './appointment/appointment.module';
+import { MedicalModule } from './medical/medical.module';
+import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, AppointmentModule, MedicalModule, ReportModule],
   controllers: [AppController],
   providers: [AppService],
 })
