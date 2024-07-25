a56e550d207ab579ec0b930812b395100f5d1ea6
arthursvpb <arthurandroid15@gmail.com>
2024-07-24 21:04:40 -0300

    Add config module for security

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,17 @@ import { UserModule } from './user/user.module';
 import { AppointmentModule } from './appointment/appointment.module';
 import { MedicalModule } from './medical/medical.module';
 import { ReportModule } from './report/report.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, AppointmentModule, MedicalModule, ReportModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    AppointmentModule,
+    MedicalModule,
+    ReportModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
