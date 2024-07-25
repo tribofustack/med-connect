409ec58250957811626f326aeafe4b33d252ee09
arthursvpb <arthurandroid15@gmail.com>
2024-07-24 21:12:14 -0300

    Add config module integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,36 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { AppointmentModule } from './appointment/appointment.module';
 import { MedicalModule } from './medical/medical.module';
 import { ReportModule } from './report/report.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // Set to false in production
+      }),
+      inject: [ConfigService],
+    }),
     AuthModule,
     UserModule,
     AppointmentModule,
     MedicalModule,
     ReportModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
